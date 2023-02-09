4ed6ea3a15cd3dc0cd247975037831a86dfeae7a
shellyalmo <shellyalmo@gmail.com>
2023-02-09 20:32:38 +0200

    first commit

---
+++ continents.js
@@ -0,0 +1,42 @@
+class Continents {
+  constructor() {
+    document
+      .querySelectorAll(".continent")
+      .forEach((btn) =>
+        btn.addEventListener("click", countries.displayCountries)
+      );
+  }
+  async getCountriesByContinent(continentName) {
+    try {
+      const response = await fetch(
+        `https://restcountries.com/v2/region/${continentName}`
+      );
+      const countriesArray = await response.json();
+      return countriesArray;
+    } catch (error) {}
+  }
+}
+
+class Countries {
+  constructor() {
+    this.countryContainer = document.querySelector(".countries-container");
+  }
+  renderCountries(countriesArray) {
+    this.countryContainer.innerHTML = "";
+    countriesArray.map((country) => {
+      const countryDiv = document.createElement("button");
+      countryDiv.innerText = country.name;
+      this.countryContainer.appendChild(countryDiv);
+    });
+  }
+  //arrow function to bind this to countries class and not the btn
+  displayCountries = async (e) => {
+    const allCountries = await continents.getCountriesByContinent(
+      e.target.innerText
+    );
+    this.renderCountries(allCountries);
+  };
+}
+
+const countries = new Countries();
+const continents = new Continents();
